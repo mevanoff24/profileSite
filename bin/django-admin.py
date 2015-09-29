8809df53883a6ded553a5df6489226ca999e5c6c
Matt Evanoff <mevanoff24@gmail.com>
2015-09-29 12:13:27 -0700

    first commit

---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/mattevanoff/Desktop/DataScientist/personal_site/profileSite/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ lib/python2.7/abc.py
@@ -0,0 +1,1 @@
+/Users/mattevanoff/anaconda/lib/python2.7/abc.py
