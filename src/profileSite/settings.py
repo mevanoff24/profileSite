307974eb2260c3f10f1b555ab920a7cd0cb2e773
Matt Evanoff <mevanoff24@gmail.com>
2015-10-01 19:46:06 -0700

    project page

--- src/profileSite/settings.py
+++ src/profileSite/settings.py
@@ -29,8 +29,8 @@ ALLOWED_HOSTS = []
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'mevanoff24@gmail.com'
-EMAIL_HOST_PASSWORD = 'Baseball24'
-EMAIL_PORT = 587
+EMAIL_HOST_PASSWORD = 'Baseball1824'
+EMAIL_PORT = '587'
 EMAIL_USE_TLS = True
 
 
